e4449abcaab34f9d8539ad707f9ab1085f251a6c
LowieVanGorp <74402688+lowieCOOL@users.noreply.github.com>
2026-03-17 16:12:05 +0100

    Fix bugs in inputhandling and add ability to get hoogte and set setpoint

--- inputHandling.py
+++ inputHandling.py
@@ -7,10 +7,15 @@ import keyboard
 left_held = False
 right_held = False
 BPM = 0
+hoogte = 0
+port = 'COM7'  # Update this to your actual port
+baudrate = 9600
+setpoint = 0
 
-def init():
-    global left_held, right_held, BPM
-    ser = serial.Serial(port='COM7', baudrate=9600, timeout=0.5)
+def init(logging=False):
+    global left_held, right_held, BPM, hoogte, setpoint
+    ser = serial.Serial(port=port, baudrate=baudrate, timeout=0.5)
+    set_setpoint = 0
     if (ser.is_open):
         print("Connectie geslaagd")
 
@@ -27,14 +32,16 @@ def init():
         waiting_above_threshold = False
         last_peak_time = 0
         last_beats = []
+
         
         # Example regex to capture the number after 'hartslag:'
         # It looks for 'hartslag:' followed by optional spaces and then digits
         pattern_hartslag = re.compile(r"hartslag:\s*(\d+)")
         pattern_laser1 = re.compile(r"laser1:\s*(\d+)")
         pattern_laser2 = re.compile(r"laser2:\s*(\d+)")
+        pattern_hogote = re.compile(r"hoogte:\s*(\d+)")
 
-        def process_heartrate(line_str):
+        def process_heartrate(line_str, on_leading_edge, waiting_above_threshold, last_peak_time, last_beats):
             match = pattern_hartslag.search(line_str)
             if match:
                 val = int(match.group(1))
@@ -57,30 +64,42 @@ def init():
                 threshold_data.append(threshold)
 
                 time_since_last_peak = time.time() - last_peak_time
-                if val > threshold:
-                    if not waiting_above_threshold and not y_data[-2] - y_data[-1] > 0: # Check for leading edge
+                if val > threshold and (peak_data[-1] - avg_data[-1]) > 80: # Check for leading edge
+                    if waiting_above_threshold or y_data[-1] - y_data[-2] < 0: # Check for leading edge
+                        pass
+                        #on_leading_edge = True
+                    else:
                         if on_leading_edge:
-                            if time_since_last_peak < 2.5: # Minimum time between peaks (e.g., 500ms)
+                            if time_since_last_peak < 2.5: # Minimum time between peaks
                                 last_beats.append(time_since_last_peak)
                                 if len(last_beats) > 5: # Keep only the last 5 beats
                                     last_beats.pop(0)
                             waiting_above_threshold = True
                             last_peak_time = time.time()
                             on_leading_edge = False
-                    else:
-                        on_leading_edge = True
                 else:
                     waiting_above_threshold = False
+                    on_leading_edge = True
                     if time_since_last_peak > 2.5: # Reset if too much time has passed without a new peak
                         last_beats = []
+                    # else:
+                    #     if on_leading_edge: # Only count as a beat if we were on a leading edge
+                    #         last_beats.append(time_since_last_peak)
+                    #         if len(last_beats) > 5: # Keep only the last 5 beats
+                    #             last_beats.pop(0)
+                    #         last_peak_time = time.time()
+                    #         beat_indices.append(len(y_data) - 1)
+                    #         beat_values.append(val)
+                    #         on_leading_edge = False
                 if len(last_beats) > 0:
                     BPM = 60 / np.mean(last_beats) if last_beats else 0
                     BPM_data.append(BPM)
-                    print(f"Current BPM: {BPM:.2f}")
+                    # print(f"Current BPM: {BPM:.2f}")
                 else:
+                    BPM = 0
                     BPM_data.append(0)
-                return BPM_data[-1]
-            return None
+                return BPM, on_leading_edge, waiting_above_threshold, last_peak_time, last_beats
+            return 0, on_leading_edge, waiting_above_threshold, last_peak_time, last_beats
 
         def process_lasers(line_str):
             match1 = pattern_laser1.search(line_str)
@@ -92,12 +111,20 @@ def init():
                 laser2_val = int(match2.group(1))
             laser1_pressed, laser2_pressed = False, False
             # print(f"Laser 1: {laser1_val}, Laser 2: {laser2_val}")
-            if laser1_val > 150: # Example threshold for laser 1
+            if laser1_val > 300: # Example threshold for laser 1
                 laser1_pressed = True
-            if laser2_val > 300: # Example threshold for laser 2
+            if laser2_val > 150: # Example threshold for laser 2
                 laser2_pressed = True
             # print(f"Laser 1: {'Pressed' if laser1_pressed else 'Not Pressed'}, Laser 2: {'Pressed' if laser2_pressed else 'Not Pressed'}")
             return laser1_pressed, laser2_pressed
+        
+        def process_hoogte(line_str):
+            match = pattern_hogote.search(line_str)
+            if match:
+                val = int(match.group(1))
+                # print(f"Hoogte: {val}")
+                return val
+            return 0
 
         try:
             
@@ -110,8 +137,11 @@ def init():
                         #print(line_str)
                         
                         # Check for the specific pattern
-                        #process_heartrate(line_str)
+                        BPM, on_leading_edge, waiting_above_threshold, last_peak_time, last_beats = process_heartrate(line_str, on_leading_edge, waiting_above_threshold, last_peak_time, last_beats)
                         left_pressed, right_pressed = process_lasers(line_str)
+                        temp_hoogte = process_hoogte(line_str)
+                        if temp_hoogte > 0:
+                            hoogte = temp_hoogte
 
                         if left_pressed and not left_held:
                             keyboard.press('left')
@@ -126,6 +156,14 @@ def init():
                         elif not right_pressed and right_held:
                             keyboard.release('right')
                             right_held = False
+
+                        if set_setpoint != setpoint:
+                            set_setpoint = setpoint
+                            ser.write(f's{int(setpoint)}\n'.encode('ascii'))
+                            # print(f"Tube setpoint updated to: {setpoint}")
+
+                        if logging:
+                            print(f"Heart Rate: {BPM:.2f} BPM, Laser 1: {'Pressed' if left_pressed else 'Not Pressed'}, Laser 2: {'Pressed' if right_pressed else 'Not Pressed'}")
 
                     except ValueError:
                         pass # Skip if parsing fails
@@ -145,8 +183,19 @@ def init():
 
     ser.close()
 
+def sendSetpoint(value):
+    global setpoint
+    setpoint = value
+    # print(f"Setpoint updated to: {setpoint}")
+
 def getBPM():
     return BPM
 
 def getPresses():
     return left_held, right_held
+
+def getHoogte():
+    return hoogte
+
+if __name__ == "__main__":
+    init(logging=True)
